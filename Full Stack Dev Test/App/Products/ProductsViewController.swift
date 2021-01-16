90b33a53bf4af7c8101c162a4e079523485847a3
Joseph <joseph@Josephs-Mac-mini.local>
2021-01-16 11:04:14 +0800

    added product codable struct

--- Full Stack Dev Test/App/Products/ProductsViewController.swift
+++ Full Stack Dev Test/App/Products/ProductsViewController.swift
@@ -14,6 +14,20 @@ class ProductsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    
+    func loadJson(jsonName: String) -> [Codable]{
+        var jsonData: [Codable] = []
+        guard let filePath = Bundle.main.path(forResource: "products", ofType: "json") else { return [] }
+        guard let data = try? Data(referencing: NSData(contentsOfFile: filePath)) else { return [] }
+        do {
+            jsonData = try JSONDecoder().decode([Currency].self, from: data)
+            return jsonData
+        } catch {
+            print(error.localizedDescription)
+        }
+        return jsonData
+        
+        
+    }
 }
 
---
+++ Full Stack Dev Test/Codable/ProductCodable.swift
@@ -0,0 +1,14 @@
+//
+//  ProductCodable.swift
+//  Full Stack Dev Test
+//
+//  Created by Joseph on 1/16/21.
+//
+
+import Foundation
+
+
+struct ProductCodables: Codable {
+    let id, name, category, price: String
+    let bgColor: String
+}
